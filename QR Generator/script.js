47d1eaa7b151cd932d6c759e3f097a1fcbb238b0
Areefa-Cader <areefacader12345@gmail.com>
2024-10-10 20:05:40 +0530

    QR UI

--- QR Generator/script.js
+++ QR Generator/script.js
@@ -5,7 +5,7 @@ var qrText = document.getElementById('qrText');
 function qrGenerate(){
     if(qrText.value.length > 0 ){
         qrImage.src =  "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-        imageBox.classList.add('max-h-[160px]' , 'm-[10px_auto]' , 'border-1' , 'border-[#d1d1d1]');
+        imageBox.classList.add('max-h-[300px]' , 'm-[10px_auto]' , 'border-1' , 'border-[#d1d1d1]');
     }else{
         return alert('please fill required field')
     }
