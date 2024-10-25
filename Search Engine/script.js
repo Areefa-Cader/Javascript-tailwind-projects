5426fbda159c4cad53c4ca450d2d68ee0929cede
Areefa-Cader <areefacader12345@gmail.com>
2024-10-25 21:12:07 +0530

    search engine halfway

---
+++ Search Engine/script.js
@@ -0,0 +1,4 @@
+var searchForm = document.getElementById('search-form');
+var searchBox = document.getElementById('search-box');
+var searchResult = document.getElementById('search-result');
+var showMoreBtn = document.getElementById('show-more-btn');
